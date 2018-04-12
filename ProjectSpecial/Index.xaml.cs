010fe07f393d34de0a8a5f2adb51c30077c0bc80
xiaozhong xu <xxz0528@outlook.com>
2018-04-12 10:36:10 +0800

    start

--- ProjectSpecial/Index.xaml.cs
+++ ProjectSpecial/Index.xaml.cs
@@ -9,17 +9,16 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace ProjectSpecial
 {
     /// <summary>
-    /// MainWindow.xaml 的交互逻辑
+    /// Interaction logic for Index.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class Index : Window
     {
-        public MainWindow()
+        public Index()
         {
             InitializeComponent();
         }
